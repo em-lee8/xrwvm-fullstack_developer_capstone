25f1220eee54f3b5b62513e5674a397feb6ce1e4
em-lee8 <159493214+em-lee8@users.noreply.github.com>
2024-06-03 16:02:40 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -96,6 +96,7 @@ def registration(request):
         return JsonResponse(data)
 # # Update the `get_dealerships` view to render the index page with
 
+
 def get_dealerships(request, state="All"):
     if (state == "All"):
         endpoint = "/fetchDealers"
@@ -105,6 +106,7 @@ def get_dealerships(request, state="All"):
     return JsonResponse({"status": 200, "dealers": dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
+
 
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
@@ -121,6 +123,7 @@ def get_dealer_reviews(request, dealer_id):
 
 # Create a `get_dealer_details` view to render the dealer details
 
+
 def get_dealer_details(request, dealer_id):
     if (dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
@@ -131,11 +134,11 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 
+
 def add_review(request):
-    if(request.user.is_anonymous == False):
+    if (request.user.is_anonymous == False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({
