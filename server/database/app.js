da7d026d85f1a4387cc776ebf4f6ec4274fb4492
em-lee8 <lee888emily@gmail.com>
2024-06-03 14:58:38 -0400

    Adding temporary changes for Week 4

--- server/database/app.js
+++ server/database/app.js
@@ -5,20 +5,18 @@ const  cors = require('cors')
 const app = express()
 const port = 3030;
 
-app.use(cors());
+app.use(cors())
 app.use(require('body-parser').urlencoded({ extended: false }));
-
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
 const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
-
 
 mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
 
 
 const Reviews = require('./review');
-const Dealerships = require('./dealership');
 
+const Dealerships = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(()=>{
@@ -27,16 +25,18 @@ try {
   Dealerships.deleteMany({}).then(()=>{
     Dealerships.insertMany(dealerships_data['dealerships']);
   });
+  
 } catch (error) {
   res.status(500).json({ error: 'Error fetching documents' });
 }
 
 
+// Express route to home
 app.get('/', async (req, res) => {
     res.send("Welcome to the Mongoose API")
 });
 
-
+// Express route to fetch all reviews
 app.get('/fetchReviews', async (req, res) => {
   try {
     const documents = await Reviews.find();
@@ -46,45 +46,75 @@ app.get('/fetchReviews', async (req, res) => {
   }
 });
 
-
+// Express route to fetch reviews by a particular dealer
 app.get('/fetchReviews/dealer/:id', async (req, res) => {
-    try {
-      const documents = await Reviews.find({dealership: req.params.id});
-      res.json(documents);
-    } catch (error) {
-      res.status(500).json({ error: 'Error fetching Reviews for dealship documents' });
-    }
-  });
+  try {
+    const documents = await Reviews.find({dealership: req.params.id});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
+});
 
-
+// Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-    try {
-      const documents = await Dealerships.find();
-      res.json(documents);
-    } catch (error) {
-      res.status(500).json({ error: 'Error fetching documents' });
-    }
+//Write your code here
+try {
+    const dealers = await Dealerships.find();
+    res.json(dealers);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
-
+// Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-    try {
-      const documents = await Dealerships.find({ state: req.params.state });
-      res.json(documents);
-    } catch (error) {
-      res.status(500).json({ error: 'Error fetching documents' });
-    }
+//Write your code here
+try {
+    const dealers = await Dealerships.find({state: req.params.state});
+    res.json(dealers);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
-
+// Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-    try {
-      const documents = await Dealerships.find({ id: req.params.id });
-      res.json(documents);
-    } catch (error) {
-      res.status(500).json({ error: 'Error fetching dealers by ID' });
-    }
-  });
+//Write your code here
+try {
+    const dealers = await Dealerships.find({id: req.params.id});
+    res.json(dealers);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
+});
+
+//Express route to insert review
+app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
+  data = JSON.parse(req.body);
+  const documents = await Reviews.find().sort( { id: -1 } )
+  let new_id = documents[0]['id']+1
+
+  const review = new Reviews({
+		"id": new_id,
+		"name": data['name'],
+		"dealership": data['dealership'],
+		"review": data['review'],
+		"purchase": data['purchase'],
+		"purchase_date": data['purchase_date'],
+		"car_make": data['car_make'],
+		"car_model": data['car_model'],
+		"car_year": data['car_year'],
+	});
+
+  try {
+    const savedReview = await review.save();
+    res.json(savedReview);
+  } catch (error) {
+		console.log(error);
+    res.status(500).json({ error: 'Error inserting review' });
+  }
+});
 
 // Start the Express server
 app.listen(port, () => {
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -3,6 +3,7 @@ import requests
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 backend_url = os.getenv(
@@ -11,6 +12,8 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+# def get_request(endpoint, **kwargs):
+# Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
     if(kwargs):
@@ -28,6 +31,9 @@ def get_request(endpoint, **kwargs):
         # If any error occurs
         print("Network exception occurred")
 
+# def analyze_review_sentiments(text):
+# request_url = sentiment_analyzer_url+"analyze/"+text
+# Add code for retrieving sentiments
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
@@ -38,6 +44,8 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+# def post_review(data_dict):
+# Add code for posting review
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
